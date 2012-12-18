2f0412e6715e5a64d97d6a8c58f6e14abdb6d477
Jay Pipes <jaypipes@gmail.com>
2012-12-18 17:45:23 -0500

    Adds way to configure what sections of secret databag are called
    * Also adds two convenience routines for db_password and service_password

--- README.md
+++ README.md
@@ -28,6 +28,8 @@ This cookbook exposes a set of default library routines:
  * `db_uri` -- Returns the SQLAlchemy RFC-1738 DB URI (see: http://rfc.net/rfc1738.html) for a named OpenStack database
  * `db_create_with_user` -- Creates a database and database user for a named OpenStack database
  * `secret` -- Returns the value of an encrypted data bag for a named OpenStack secret key and key-section
+  * `db_password` -- Ease-of-use helper that returns the decrypted database password for a named OpenStack database
+  * `service_password` -- Ease-of-use helper that returns the decrypted service password for named OpenStack service
 
 Usage
 -----
@@ -56,10 +58,10 @@ require "uri"
 puts ::URI.decode nova_api_ap.to_s
 ```
 
-Example of using the `secret` and `db\_uri` routine:
+Example of using the `db_password` and `db_uri` routine:
 
 ```ruby
-db_pass = secret "passwords", "cinder"
+db_pass = db_password "cinder"
 db_user = node["cinder"]["db"]["user"]
 sql_connection = db_uri "volume", db_user, db_pass
 
--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -192,4 +192,42 @@ describe ::Openstack do
       result.should eq value
     end
   end
+
+  describe "#service_password" do
+    it "returns index param when developer_mode is true" do
+      @chef_run = ::ChefSpec::ChefRunner.new do |n|
+        n.set["openstack"]["developer_mode"] = true
+      end
+      @chef_run.converge "openstack-common::default"
+      @subject.stub(:node).and_return @chef_run.node
+      result = @subject.service_password("nova")
+      result.should eq "nova"
+    end
+    it "returns databag when developer_mode is false" do
+      value = "this"
+      ::Chef::EncryptedDataBagItem.stub(:load).with("service_passwords", "nova", "/etc/chef/openstack_data_bag_secret").and_return value
+      @subject.stub(:node).and_return @chef_run.node
+      result = @subject.service_password("nova")
+      result.should eq value
+    end
+  end
+
+  describe "#db_password" do
+    it "returns index param when developer_mode is true" do
+      @chef_run = ::ChefSpec::ChefRunner.new do |n|
+        n.set["openstack"]["developer_mode"] = true
+      end
+      @chef_run.converge "openstack-common::default"
+      @subject.stub(:node).and_return @chef_run.node
+      result = @subject.db_password("nova")
+      result.should eq "nova"
+    end
+    it "returns databag when developer_mode is false" do
+      value = "this"
+      ::Chef::EncryptedDataBagItem.stub(:load).with("db_passwords", "nova", "/etc/chef/openstack_data_bag_secret").and_return value
+      @subject.stub(:node).and_return @chef_run.node
+      result = @subject.db_password("nova")
+      result.should eq value
+    end
+  end
 end

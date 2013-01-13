7b5d54a3cf93cb13158744c210a91c512cb5fce4
John Dewey <john@dewey.ws>
2013-01-13 12:54:15 -0800

    Broke out default into modules after openstack-utils merge
    Since openstack-utils had things broken out, felt having things in
    default wasn't as clean.  Broke things out into modules, cleaned
    up tests, and a few rogue ()'s :).

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -8,7 +8,7 @@ describe "openstack-common::default" do
 
   describe "apt setup" do
     it "sets apt repositories correctly" do
-      Chef::Recipe.any_instance.stub(:apt_repository)
+      Chef::Recipe.any_instance.stub :apt_repository
       @chef_run = ::ChefSpec::ChefRunner.new(:log_level => :info) do |n|
         n.set["platform_family"] = "debian"
         n.set["lsb"]["codename"] = "precise"
@@ -24,138 +24,8 @@ end
 
 describe ::Openstack do
   before do
-    @chef_run = ::ChefSpec::ChefRunner.new do |n|
-      n.set['mysql'] = {}
-      n.set['mysql']['server_root_password'] = "password"
-      n.set["openstack"]["secret"]["service_passwords_data_bag"] = "service_passwords"
-      n.set["openstack"]["secret"]["db_passwords_data_bag"] = "db_passwords"
-      n.set["openstack"]["secret"]["user_passwords_data_bag"] = "user_passwords"
-    end
-    @chef_run.converge "openstack-common::default"
-    @subject = ::Object.new.extend(::Openstack)
-  end
-
-  describe "#endpoint" do
-    it "returns nil when no openstack.endpoints not in node attrs" do
-      @subject.stub(:node).and_return {}
-      @subject.endpoint("nonexisting").should be_nil
-    end
-    it "returns nil when no such endpoint was found" do
-      @subject.stub(:node).and_return @chef_run.node
-      @subject.endpoint("nonexisting").should be_nil
-    end
-    it "handles a URI needing escaped" do
-      uri_hash = {
-        "openstack" => {
-          "endpoints" => {
-            "compute-api" => {
-              "uri" => "http://localhost:8080/v2/%(tenant_id)s"
-            }
-          }
-        }
-      }
-      @subject.stub(:node).and_return uri_hash
-      result = @subject.endpoint "compute-api"
-      result.path.should == "/v2/%25(tenant_id)s"
-    end
-    it "returns endpoint URI object when uri key in endpoint hash" do
-      uri_hash = {
-        "openstack" => {
-          "endpoints" => {
-            "compute-api" => {
-              "uri" => "http://localhost:8080/path"
-            }
-          }
-        }
-      }
-      @subject.stub(:node).and_return uri_hash
-      result = @subject.endpoint "compute-api"
-      result.port.should == 8080
-    end
-    it "returns endpoint URI string when uri key in endpoint hash and host also in hash" do
-      uri_hash = {
-        "openstack" => {
-          "endpoints" => {
-            "compute-api" => {
-              "uri" => "http://localhost",
-              "host" => "ignored"
-            }
-          }
-        }
-      }
-      @subject.stub(:node).and_return uri_hash
-      @subject.endpoint("compute-api").to_s.should eq "http://localhost"
-    end
-    it "returns endpoint URI object when uri key not in endpoint hash but host is in hash" do
-      @subject.should_receive(:uri_from_hash).with({"host"=>"localhost", "port"=>"8080"})
-      uri_hash = {
-        "openstack" => {
-          "endpoints" => {
-            "compute-api" => {
-              "host" => "localhost",
-              "port" => "8080"
-            }
-          }
-        }
-      }
-      @subject.stub(:node).and_return uri_hash
-      @subject.endpoint "compute-api"
-    end
-  end
-
-  describe "#endpoints" do
-    it "does nothing when no endpoints" do
-      @subject.stub(:node).and_return {}
-      @subject.endpoints.should be_nil
-    end
-    it "does nothing when empty endpoints" do
-      @subject.stub(:node).and_return({"openstack" => { "endpoints" => {}}})
-      @count = 0
-      @subject.endpoints do | ep |
-        @count += 1
-      end
-      @count.should eq 0
-    end
-    it "executes block count when have endpoints" do
-      @subject.stub(:node).and_return @chef_run.node
-      @count = 0
-      @subject.endpoints do |ep|
-        @count += 1
-      end
-      @count.should >= 1
-    end
-  end
-
-  describe "#db" do
-    it "returns nil when no openstack.db not in node attrs" do
-      @subject.stub(:node).and_return {}
-      @subject.db("nonexisting").should be_nil
-    end
-    it "returns nil when no such service was found" do
-      @subject.stub(:node).and_return @chef_run.node
-      @subject.db("nonexisting").should be_nil
-    end
-    it "returns db info hash when service found" do
-      @subject.stub(:node).and_return @chef_run.node
-      @subject.db("compute")['host'].should == "127.0.0.1"
-      @subject.db("compute").has_key?("uri").should be_false
-    end
-  end
-
-  describe "#db_uri" do
-    it "returns nil when no openstack.db not in node attrs" do
-      @subject.stub(:node).and_return {}
-      @subject.db_uri("nonexisting", "user", "pass").should be_nil
-    end
-    it "returns nil when no such service was found" do
-      @subject.stub(:node).and_return @chef_run.node
-      @subject.db_uri("nonexisting", "user", "pass").should be_nil
-    end
-    it "returns db info hash when service found" do
-      @subject.stub(:node).and_return @chef_run.node
-      expect = "mysql://user:pass@127.0.0.1:3306/nova"
-      @subject.db_uri("compute", "user", "pass").should eq expect
-    end
+    @chef_run = ::ChefSpec::ChefRunner.new.converge "openstack-common::default"
+    @subject = ::Object.new.extend ::Openstack
   end
 
   describe "#db_create_with_user" do
@@ -171,89 +41,9 @@ describe ::Openstack do
       @subject.stub(:database).and_return {}
       @subject.stub(:database_user).and_return {}
       @subject.stub(:node).and_return @chef_run.node
-      result = @subject.db_create_with_user("compute", "user", "pass")
-      result['host'].should eq "127.0.0.1"
-      result['port'].should eq "3306"
-    end
-  end
-
-  describe "#secret" do
-    it "returns index param when developer_mode is true" do
-      @chef_run = ::ChefSpec::ChefRunner.new(:log_level => :info) do |n|
-        n.set["openstack"]["developer_mode"] = true
-      end
-      @chef_run.converge "openstack-common::default"
-      @subject.stub(:node).and_return @chef_run.node
-      result = @subject.secret("passwords", "nova")
-      result.should eq "nova"
-    end
-    it "returns databag when developer_mode is false" do
-      value = {"nova" => "this"}
-      ::Chef::EncryptedDataBagItem.stub(:load_secret).with("/etc/chef/openstack_data_bag_secret").and_return "secret"
-      ::Chef::EncryptedDataBagItem.stub(:load).with("passwords", "nova", "secret").and_return value
-      @subject.stub(:node).and_return @chef_run.node
-      result = @subject.secret("passwords", "nova")
-      result.should eq "this"
-    end
-  end
-
-  describe "#service_password" do
-    it "returns index param when developer_mode is true" do
-      @chef_run = ::ChefSpec::ChefRunner.new do |n|
-        n.set["openstack"]["developer_mode"] = true
-      end
-      @chef_run.converge "openstack-common::default"
-      @subject.stub(:node).and_return @chef_run.node
-      result = @subject.service_password("nova")
-      result.should eq "nova"
-    end
-    it "returns databag when developer_mode is false" do
-      value = {"nova" => "this"}
-      ::Chef::EncryptedDataBagItem.stub(:load_secret).with("/etc/chef/openstack_data_bag_secret").and_return "secret"
-      ::Chef::EncryptedDataBagItem.stub(:load).with("service_passwords", "nova", "secret").and_return value
-      @subject.stub(:node).and_return @chef_run.node
-      result = @subject.service_password("nova")
-      result.should eq "this"
-    end
-  end
-
-  describe "#db_password" do
-    it "returns index param when developer_mode is true" do
-      @chef_run = ::ChefSpec::ChefRunner.new do |n|
-        n.set["openstack"]["developer_mode"] = true
-      end
-      @chef_run.converge "openstack-common::default"
-      @subject.stub(:node).and_return @chef_run.node
-      result = @subject.db_password("nova")
-      result.should eq "nova"
-    end
-    it "returns databag when developer_mode is false" do
-      value = {"nova" => "this"}
-      ::Chef::EncryptedDataBagItem.stub(:load_secret).with("/etc/chef/openstack_data_bag_secret").and_return "secret"
-      ::Chef::EncryptedDataBagItem.stub(:load).with("db_passwords", "nova", "secret").and_return value
-      @subject.stub(:node).and_return @chef_run.node
-      result = @subject.db_password("nova")
-      result.should eq "this"
-    end
-  end
-
-  describe "#user_password" do
-    it "returns index param when developer_mode is true" do
-      @chef_run = ::ChefSpec::ChefRunner.new do |n|
-        n.set["openstack"]["developer_mode"] = true
-      end
-      @chef_run.converge "openstack-common::default"
-      @subject.stub(:node).and_return @chef_run.node
-      result = @subject.user_password("nova")
-      result.should eq "nova"
-    end
-    it "returns databag when developer_mode is false" do
-      value = {"nova" => "this"}
-      ::Chef::EncryptedDataBagItem.stub(:load_secret).with("/etc/chef/openstack_data_bag_secret").and_return "secret"
-      ::Chef::EncryptedDataBagItem.stub(:load).with("user_passwords", "nova", "secret").and_return value
-      @subject.stub(:node).and_return @chef_run.node
-      result = @subject.user_password("nova")
-      result.should eq "this"
+      result = @subject.db_create_with_user "compute", "user", "pass"
+      result['host'].should == "127.0.0.1"
+      result['port'].should == "3306"
     end
   end
 end
--- spec/roles_spec.rb
+++ spec/roles_spec.rb
@@ -4,34 +4,32 @@ require ::File.join ::File.dirname(__FILE__), "..", "libraries", "roles"
 
 describe ::Openstack do
   before do
-    @subject = ::Object.new.extend(::Openstack)
+    @chef_run = ::ChefSpec::ChefRunner.new.converge "openstack-common::default"
+    @subject = ::Object.new.extend ::Openstack
   end
 
   describe "#config_by_role" do
     it "returns nil when section not in node hash" do
-      node = ::ChefSpec::ChefRunner.new.converge("openstack-common::default").node
+      node = @chef_run.node
       node['roles'] << "role1"
       @subject.stub(:node).and_return node
-
       @subject.config_by_role("role1", "foo").should be_nil
     end
     it "returns section when section in node hash" do
       ::Chef::Search::Query.stub(:new)
-      node = ::ChefSpec::ChefRunner.new.converge("openstack-common::default").node
+      node = @chef_run.node
       node['roles'] << "role1"
       node.set['foo'] = "bar"
       @subject.stub(:node).and_return node
-
       @subject.config_by_role("role1", "foo").should == "bar"
     end
     it "returns nil when no such role found" do
       ::Chef::Search::Query.stub(:new).with(:search).and_return(
         [ [], nil, nil ]
       )
-      node = ::ChefSpec::ChefRunner.new.converge("openstack-common::default").node
+      node = @chef_run.node
       node['roles'] << "role1"
       @subject.stub(:node).and_return node
-
       @subject.config_by_role("role1", "bar").should be_nil
     end
     it "returns section when section in first search result" do
@@ -40,9 +38,7 @@ describe ::Openstack do
           [ { "foo" => "bar" } ], nil, nil
         ]
       )
-      node = ::ChefSpec::ChefRunner.new.converge("openstack-common::default").node
-      @subject.stub(:node).and_return node
-
+      @subject.stub(:node).and_return @chef_run.node
       @subject.config_by_role("role1", "foo").should == "bar"
     end
     it "returns full node hash when search match but no section supplied" do
@@ -51,9 +47,7 @@ describe ::Openstack do
           [ { "foo" => "bar" } ], nil, nil
         ]
       )
-      node = ::ChefSpec::ChefRunner.new.converge("openstack-common::default").node
-      @subject.stub(:node).and_return node
-
+      @subject.stub(:node).and_return @chef_run.node
       @subject.config_by_role("role1").should == { "foo" => "bar" }
     end
   end

4e2bbdc15d21271259e3cbd61d9f99a2d72c564f
Yuriy Taraday <yorik.sar@gmail.com>
2013-01-29 23:59:24 +0400

    Add solo support to memcached_servers
    (for foodcritic)

--- libraries/endpoints.rb
+++ libraries/endpoints.rb
@@ -78,8 +78,16 @@ module ::Openstack
   # env - sets environment where to search
   # role - sets role that is used to filter out memcached nodes
   def memcached_servers(env=node.chef_environment, role="infra-caching")
-    search(:node, "chef_environment:#{env} AND roles:#{role}").map do |c_node|
-      "#{c_node['memcached']['listen']}:11211"
+    if Chef::Config[:solo]
+      if node['memcached']
+        ["#{node['memcached']['listen']}:11211"]
+      else
+        []
+      end
+    else
+      search(:node, "chef_environment:#{env} AND roles:#{role}").map do |c_node|
+        "#{c_node['memcached']['listen']}:11211"
+      end
     end
   end
 

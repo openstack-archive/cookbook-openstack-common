0fa1154758b66178ba24fe262bdfcd5f13405a43
Matt Ray <matthewhray@gmail.com>
2014-01-08 16:17:27 -0600

    Filling out initial rubocop support.
    https://blueprints.launchpad.net/openstack-chef/+spec/lint-and-unit-testing-for-havana
    Removing tailor, cleaning up metadata and Gemfile and adding TESTING.md
    
    Change-Id: I193388f59f3265a6a549452fd798ee512f197603

--- metadata.rb
+++ metadata.rb
@@ -1,19 +1,19 @@
-name             "openstack-common"
-maintainer       "AT&T Services, Inc."
-maintainer_email "cookbooks@lists.tfoundry.com"
-license          "Apache 2.0"
-description      "Common OpenStack attributes, libraries and recipes."
+name             'openstack-common'
+maintainer       'AT&T Services, Inc.'
+maintainer_email 'cookbooks@lists.tfoundry.com'
+license          'Apache 2.0'
+description      'Common OpenStack attributes, libraries and recipes.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "8.0.1"
+version          '8.0.1'
 
-recipe           "openstack-common", "Installs/Configures common recipes"
-recipe           "openstack-common::set_endpoints_by_interface", "Set endpoints by interface"
-recipe           "openstack-common::logging", "Installs/Configures common logging"
-recipe           "openstack-common::sysctl", "Configures sysctl settings"
+recipe           'openstack-common', 'Installs/Configures common recipes'
+recipe           'openstack-common::set_endpoints_by_interface', 'Set endpoints by interface'
+recipe           'openstack-common::logging', 'Installs/Configures common logging'
+recipe           'openstack-common::sysctl', 'Configures sysctl settings'
 
 %w{ ubuntu suse }.each do |os|
   supports os
 end
 
-depends          "apt"
-depends          "database"
+depends          'apt'
+depends          'database'

ad0f56149d8805537446e68275e27a11094e280f
JJ Asghar <jj@getchef.com>
2014-10-07 22:15:03 -0500

    Updating the RDO location for the URL
    - They moved the URL to "icehouse" instead of master.
    
    Change-Id: I30f9a9546a3c28bd3682e460dc6bc30274609c99

--- attributes/default.rb
+++ attributes/default.rb
@@ -115,7 +115,7 @@ default['openstack']['zypp']['uri'] = 'http://download.opensuse.org/repositories
 
 default['openstack']['yum']['rdo_enabled'] = true
 default['openstack']['yum']['uri'] = 'http://repos.fedorapeople.org/repos/openstack/openstack-icehouse/epel-6'
-default['openstack']['yum']['repo-key'] = 'https://raw.githubusercontent.com/redhat-openstack/rdo-release/master/RPM-GPG-KEY-RDO-Icehouse'
+default['openstack']['yum']['repo-key'] = 'https://raw.githubusercontent.com/redhat-openstack/rdo-release/icehouse/RPM-GPG-KEY-RDO-Icehouse'
 
 # ======================== OpenStack Endpoints ================================
 #

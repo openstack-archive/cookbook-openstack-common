28ef3e2fe5fb24415b345644b5771c7f03d7fe36
Jens Rosenboom <j.rosenboom@x-ion.de>
2017-05-24 09:26:30 +0000

    Update OSC client version to 3.11.0
    The previous version is no longer working after python-novaclient got
    upgraded to 8.0.0. There is also no proper stable version, so update to
    current latest.
    
    Change-Id: If4a86dfd16c9abbdb0957520d30b7e5c1435d4c1

--- attributes/default.rb
+++ attributes/default.rb
@@ -27,7 +27,7 @@ default['openstack']['common']['custom_template_banner'] = '
 '
 
 # version for python-openstackclient
-default['openstack']['common']['client_version'] = '3.6.0'
+default['openstack']['common']['client_version'] = '3.11.0'
 
 # OpenStack services and their project names
 default['openstack']['common']['services'] = {

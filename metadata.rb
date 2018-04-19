62d5cd52752beb6c93a9e22de17435f55c7488b2
Samuel Cassiba <s@cassiba.com>
2018-04-19 05:55:12 -0700

    Update poise-python to latest stable
    *poise received a round of updates, so let's remove the funky workaround.
    *update Berksfile.lock to allow usage of new poise-python
    
    Change-Id: I657bfd5459f6973a4ae420bf9b5c87039ab1529d

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'openstack-dev@lists.openstack.org'
 license          'Apache 2.0'
 description      'Common OpenStack attributes, libraries and recipes.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '15.1.0'
+version          '15.1.1'
 
 recipe 'openstack-common', 'Installs/Configures common recipes'
 recipe 'openstack-common::logging', 'Installs/Configures common logging'
@@ -20,7 +20,7 @@ depends 'mariadb', '~> 1.5'
 depends 'mysql', '~> 8.2'
 depends 'yum', '~> 3.13'
 depends 'yum-epel', '~> 2.0'
-depends 'poise-python', '~> 1.5'
+depends 'poise-python', '~> 1.7'
 
 issues_url 'https://launchpad.net/openstack-chef' if respond_to?(:issues_url)
 source_url 'https://github.com/openstack/cookbook-openstack-common' if respond_to?(:source_url)

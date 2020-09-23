f807a66739ca621d48e052d595171574cdb0c2ac
Lance Albertson <lance@osuosl.org>
2020-09-23 09:36:32 -0700

    Release 19.2.1
    I forgot to bump the release in https://review.opendev.org/753416.
    
    Change-Id: I75e50e2ff8f312ad183be372e6b20009c5999f97
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'openstack-chef'
 maintainer_email 'openstack-discuss@lists.openstack.org'
 license          'Apache-2.0'
 description      'Common OpenStack attributes, libraries and recipes.'
-version          '19.2.0'
+version          '19.2.1'
 
 %w(ubuntu redhat centos).each do |os|
   supports os
